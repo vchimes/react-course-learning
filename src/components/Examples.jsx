2f9afd62c94e42b2cedb717dc84c1b1ecbe84818
Vince Chimento <vchimento@gmail.com>
2025-03-19 14:02:50 -0400

    create Section component, implement Props Forwarding pattern

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import TabButton from './TabButton.jsx';
+import Section from './Section.jsx';
 import { EXAMPLES } from '../data.js';
 
 export default function Examples() {
@@ -13,30 +14,30 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
+    <Section id="examples">
       <h2>Examples</h2>
       <menu>
         <TabButton
           isSelected={selectedTopic === 'components'}
-          onSelect={() => handleSelect('components')}
+          onClick={() => handleSelect('components')}
         >
           Components
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'jsx'}
-          onSelect={() => handleSelect('jsx')}
+          onClick={() => handleSelect('jsx')}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'props'}
-          onSelect={() => handleSelect('props')}
+          onClick={() => handleSelect('props')}
         >
           Props
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'state'}
-          onSelect={() => handleSelect('state')}
+          onClick={() => handleSelect('state')}
         >
           State
         </TabButton>
@@ -52,6 +53,6 @@ export default function Examples() {
           </pre>
         </div>
       )}
-    </section>
+    </Section>
   );
 }
